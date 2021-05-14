1f9beffe26fa45f316920bc9e1e40833767edb4b
mariano <mcabezas@eia.udg.edu>
2021-05-14 11:36:54 +1000

    - Updated the pretrained model training

--- criteria.py
+++ criteria.py
@@ -53,10 +53,9 @@ def dsc_binary_loss(pred, target):
 
     dsc_k = intersection / (sum_pred + sum_target)
     dsc_k = dsc_k[torch.logical_not(torch.isnan(dsc_k))]
-    print(dsc_k)
     if len(dsc_k) > 0:
         dsc = 1 - torch.mean(dsc_k)
     else:
-        dsc = 0
+        dsc = torch.tensor(0)
 
     return torch.clamp(dsc, 0., 1.)
