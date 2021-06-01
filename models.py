6e841838f82d64605e32a41dc251fe9318c33a09
mariano <mcabezas@eia.udg.edu>
2021-06-01 11:31:12 +1000

    - changed testing print

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ def norm_f(n_f):
 
 
 def print_batch(pi, n_patches, i, n_cases, t_in, t_case_in):
-    init_c = '\033[0m'
+    init_c = '\033[38;5;238m'
     percent = 25 * (i + 1) // n_patches
     progress_s = ''.join(['█'] * percent)
     remainder_s = ''.join([' '] * (25 - percent))
